d094ce89a5ec0ce1447f196db0e6283097650f18
Lukasz-Se <lukasz.semp@gmail.com>
2025-02-07 17:55:33 +0100

    Create vowel.cpp

---
+++ homework/remove-vowels/vowel.cpp
@@ -0,0 +1,21 @@
+#include "vowel.h"
+
+std::vector<std::string> removeVowels(std::vector<std::string>& input)
+{
+	std::string vowels = "aeyuio";
+	std::vector<std::string> output;
+
+	for (auto word : input)
+	{
+		for (int letter_in_word = 0; letter_in_word < word.length(); letter_in_word++)
+			for (auto vowel : vowels)
+				if (word[letter_in_word] == vowel)
+				{
+					word.erase(word.begin() + letter_in_word);
+					letter_in_word--;
+					break;
+				}
+		output.push_back(word);
+	}
+	return output;
+}
