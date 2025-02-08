32a5f7175f27bd788397dffe5acbfa7cabff81e5
Lukasz-Se <lukasz.semp@gmail.com>
2025-02-08 14:45:50 +0100

    Update vowel.cpp

--- homework/remove-vowels/vowel.cpp
+++ homework/remove-vowels/vowel.cpp
@@ -2,18 +2,18 @@
 
 void removeVowels(std::vector<std::string>& input)
 {
-	std::string vowels = "aeiouyAEIOUY";
-	
-	for (auto &word : input)
-	{
-		for (int letter_in_word = 0; letter_in_word < word.length(); letter_in_word++)
-			for (auto vowel : vowels)
-				if (word[letter_in_word] == vowel)
-				{
-					word.erase(word.begin() + letter_in_word);
-					letter_in_word--;
-					break;
-				}
-	}
-	input.shrink_to_fit();
+    std::string vowels = "aeiouyAEIOUY";
+    
+    for (auto &word : input)
+    {
+        for (int letter_in_word = 0; letter_in_word < word.length(); letter_in_word++)
+            for (auto vowel : vowels)
+                if (word[letter_in_word] == vowel)
+                {
+                    word.erase(word.begin() + letter_in_word);
+                    letter_in_word--;
+                    break;
+                }
+    }
+    input.shrink_to_fit();
 }
