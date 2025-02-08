bac1470b8caf1533c687366d0322cad624eb7280
Lukasz-Se <lukasz.semp@gmail.com>
2025-02-08 15:39:49 +0100

    Update sort.cpp

--- homework/length-sort/sort.cpp
+++ homework/length-sort/sort.cpp
@@ -1,15 +1,14 @@
 #include "sort.hpp"
-
 using namespace std;
 
-deque<string> lengthSort(forward_list<string>& input)
+deque<string> lengthSort(forward_list<string>& input) 
 {
-	deque<string> temp;
-	for (auto element : input)
-		temp.push_back(element);
+    deque<string> temp;
+    for (auto element : input)
+        temp.push_back(element);
 
-	sort(temp.begin(), temp.end());
-	sort(temp.begin(), temp.end(), [](const auto& a, const auto& b) {return a.length() < b.length(); });
+    sort(temp.begin(), temp.end());
+    sort(temp.begin(), temp.end(), [](const auto& a, const auto& b) { return a.length() < b.length(); });
 
-	return temp;
+    return temp;
 }
