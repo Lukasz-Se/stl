79fd6a84d64d90eee37b715ff7ec122bddeac6c6
Lukasz-Se <lukasz.semp@gmail.com>
2025-02-08 14:28:11 +0100

    Update compression.h

--- homework/grayscale-image/compression.h
+++ homework/grayscale-image/compression.h
@@ -5,7 +5,7 @@
 #include <algorithm>
 #include <cstdint>
 
-constexpr size_t width = 10;
-constexpr size_t height = 3;
+constexpr size_t width = 32;
+constexpr size_t height = 32;
 
 std::vector<std::pair<uint8_t, uint8_t>> compressGrayscale(std::array<std::array<uint8_t, width>, height>& bitmap);
