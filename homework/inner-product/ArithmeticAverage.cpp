116227dad336ad4c66d654e0b5779fe890efaf4b
Lukasz-Se <lukasz.semp@gmail.com>
2025-04-18 16:04:34 +0200

    Update ArithmeticAverage.cpp

--- homework/inner-product/ArithmeticAverage.cpp
+++ homework/inner-product/ArithmeticAverage.cpp
@@ -1,23 +1,20 @@
 #include "ArithmeticAverage.h"
 
-double ArithmeticAverage(const std::vector<int>& first, const std::vector<int>& second)
-{
-	double SumOfFirstVec = std::accumulate(begin(first), end(first), 0);
-	double SumOfSecondVec = std::accumulate(begin(second), end(second), 0);
-	double SumOfElements = first.size() + second.size();
+double ArithmeticAverage(const std::vector<int>& first, const std::vector<int>& second) {
+    double SumOfFirstVec = std::accumulate(begin(first), end(first), 0);
+    double SumOfSecondVec = std::accumulate(begin(second), end(second), 0);
+    double SumOfElements = first.size() + second.size();
 
-	return (SumOfFirstVec + SumOfSecondVec) / SumOfElements;
+    return (SumOfFirstVec + SumOfSecondVec) / SumOfElements;
 }
 
-double Distance(const std::vector<int>& first, const std::vector<int>& second)
-{
-	if (first.size() == second.size())
-	{
-		std::vector<int> temp(first.size());
-		std::transform(begin(first), end(first), begin(second), begin(temp), std::minus<int>());
-		std::transform(begin(temp), end(temp), begin(temp), [](int& number) { return pow(number, 2); });
+double Distance(const std::vector<int>& first, const std::vector<int>& second) {
+    if (first.size() == second.size()) {
+        std::vector<int> temp(first.size());
+        std::transform(begin(first), end(first), begin(second), begin(temp), std::minus<int>());
+        std::transform(begin(temp), end(temp), begin(temp), [](int& number) { return pow(number, 2); });
 
-		return sqrt(std::accumulate(begin(temp), end(temp), 0));
-	}
-	return -1.0;
+        return sqrt(std::accumulate(begin(temp), end(temp), 0));
+    }
+    return -1.0;
 }
