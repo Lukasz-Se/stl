f9f8a344f07de1a724905343e17e7542ab6cc26f
Lukasz-Se <lukasz.semp@gmail.com>
2025-04-18 10:39:50 +0200

    Create main.cpp

---
+++ homework/inner-product/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+double ArithmeticAverage(const std::vector<int>& first, const std::vector<int>& second)
+{
+	double SumOfFirstVec = std::accumulate(begin(first), end(first), 0);
+	double SumOfSecondVec = std::accumulate(begin(second), end(second), 0);
+	double SumOfElements = first.size() + second.size();
+
+	return (SumOfFirstVec + SumOfSecondVec) / SumOfElements;
+}
+
+double Distance(const std::vector<int>& first, const std::vector<int>& second)
+{
+	if (first.size() == second.size())
+	{
+		std::vector<int> temp(first.size());
+		std::transform(begin(first), end(first), begin(second), begin(temp), std::minus<int>());
+		std::transform(begin(temp), end(temp), begin(temp), [](int& number) { return std::pow(number, 2); });
+
+		return std::sqrt(std::accumulate(begin(temp), end(temp), 0));
+	}
+	return -1.0;
+}
