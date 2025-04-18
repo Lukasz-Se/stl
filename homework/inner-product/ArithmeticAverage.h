72d92f022127f844ddddfa60b58575532ff3470e
Lukasz-Se <lukasz.semp@gmail.com>
2025-04-18 12:27:33 +0200

    Update ArithmeticAverage.h

--- homework/inner-product/ArithmeticAverage.h
+++ homework/inner-product/ArithmeticAverage.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <algorithm>
 #include <numeric>
+#include <cmath>
 
 double ArithmeticAverage(const std::vector<int> & first, const std::vector<int> & second);
 double Distance(const std::vector<int> & first, const std::vector<int> & second);
