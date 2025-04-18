215391380d8805fa085ff9d05975f606d1705be5
Lukasz-Se <lukasz.semp@gmail.com>
2025-04-18 11:28:36 +0200

    Add files via upload

---
+++ homework/inner-product/ArithmeticAverage.h
@@ -0,0 +1,12 @@
+#ifndef ARITHMETIC_AVERAGE_H
+#define ARITHMETIC_AVERAGE_H
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+double ArithmeticAverage(const std::vector<int> & first, const std::vector<int> & second);
+double Distance(const std::vector<int> & first, const std::vector<int> & second);
+
+#endif
