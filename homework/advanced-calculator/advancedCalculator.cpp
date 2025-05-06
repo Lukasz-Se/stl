428f910d70475e03c53bc0bb11fc5c1abdf694c6
Lukasz-Se <lukasz.semp@gmail.com>
2025-05-06 14:23:22 +0200

    Update advancedCalculator.cpp

--- homework/advanced-calculator/advancedCalculator.cpp
+++ homework/advanced-calculator/advancedCalculator.cpp
@@ -45,6 +45,23 @@ ErrorCode CheckBadChars(std::string* input, const std::string& symbols) {
 	return ErrorCode::OK;
 }
 
+ErrorCode CheckArithemticErrors(const std::string& raw_input, const Data& input_data_structure)
+{
+	if (input_data_structure.operation == '%')
+		if (!isInteger(input_data_structure.second_value) || input_data_structure.second_value == 0)
+			return ErrorCode::ModuleOfNonIntegerValue;
+
+	if (input_data_structure.operation == '/' && input_data_structure.second_value == 0)
+		return ErrorCode::DivideBy0;
+
+	if (input_data_structure.operation == '$')
+		if (input_data_structure.first_value < 0)
+			return ErrorCode::SqrtOfNegativeNumber;
+
+	if (!std::all_of(begin(raw_input), end(raw_input), [](char c) {return c == ' '; }))
+		return ErrorCode::BadFormat;
+}
+
 ErrorCode SeparateOperands(std::string& input, const std::string& allowedOps, Data* output) {
 	std::string temp;
 
@@ -57,7 +74,7 @@ ErrorCode SeparateOperands(std::string& input, const std::string& allowedOps, Da
 	
 	if (!isItCorrectNumber(temp))
 		return ErrorCode::BadFormat;
-	stringToDouble(temp, &output->frist_value);
+	stringToDouble(temp, &output->first_value);
 
 	std::transform(begin(input), it, begin(input), [](char c) {return ' '; });
 
@@ -70,26 +87,7 @@ ErrorCode SeparateOperands(std::string& input, const std::string& allowedOps, Da
 			return ErrorCode::BadFormat;
 		stringToDouble(temp, &output->second_value);
 		std::transform(it + 1, end(input), it + 1, [](char c) {return ' '; });
-	}
-
-	std::cout << "raw input:" << input << std::endl;
-
-	//if (std::any_of(begin(input), end(input), [](char c) {return c == ','; }))
-	//	return ErrorCode::BadFormat;
-
-	if (output->operation == '%')
-		if(!isInteger(output->second_value) || output->second_value == 0)
-			return ErrorCode::ModuleOfNonIntegerValue;
-
-	if (output->operation == '/' && output->second_value == 0)
-		return ErrorCode::DivideBy0;
-
-	if (output->operation == '$')
-		if (output->frist_value < 0)
-			return ErrorCode::SqrtOfNegativeNumber;
-
-	if (!std::all_of(begin(input), end(input), [](char c) {return c == ' '; }))
-		return ErrorCode::BadFormat;
+	}	
 
 	return ErrorCode::OK;
 }
@@ -113,6 +111,10 @@ ErrorCode process(std::string input, double* out) {
 	if (result != ErrorCode::OK)
 		return result;
 
+	result = CheckArithemticErrors(input, data);
+	if (result != ErrorCode::OK)
+	return result;
+	
 	*out = operations.find(data.operation)->second(data.frist_value, data.second_value);
 
 	return ErrorCode::OK;
