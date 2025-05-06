b166126b157a833aefd9c9d1d2dd4a5b9de664ff
Lukasz-Se <lukasz.semp@gmail.com>
2025-05-06 14:34:57 +0200

    Update advancedCalculator.h

--- homework/advanced-calculator/advancedCalculator.h
+++ homework/advanced-calculator/advancedCalculator.h
@@ -1,39 +1,37 @@
 #pragma once
+#include <algorithm>
+#include <cmath>
+#include <functional>
 #include <iostream>
 #include <map>
-#include <functional>
-#include <algorithm>
 #include <sstream>
-#include <cmath>
 
-enum class ErrorCode
-{
-	OK = 0,
-	BadCharacter,
-	BadFormat,
-	DivideBy0,
-	SqrtOfNegativeNumber,
-	ModuleOfNonIntegerValue
+enum class ErrorCode {
+    OK = 0,
+    BadCharacter,
+    BadFormat,
+    DivideBy0,
+    SqrtOfNegativeNumber,
+    ModuleOfNonIntegerValue
 };
 
 struct Data {
-	char operation;
-	double first_value = 0.0;
-	double second_value = 0.0;
+    char operation;
+    double first_value = 0.0;
+    double second_value = 0.0;
 };
 
 const std::map<const char, std::function<double(double, double)>> operations{
-	{'+', std::plus<double>()},
-	{'-', std::minus<double>()},
-	{'*', std::multiplies<double>()},
-	{'/', std::divides<double>()},
-	{'%', std::modulus<int>()},
-	{'!', [](auto base, auto empty) {if (base >= 0)
+    {'+', std::plus<double>()},
+    {'-', std::minus<double>()},
+    {'*', std::multiplies<double>()},
+    {'/', std::divides<double>()},
+    {'%', std::modulus<int>()},
+    {'!', [](auto base, auto empty) {if (base >= 0)
 	return tgamma(base + 1);
 	else return -tgamma(-base + 1); }},
-	{'^', [](auto base, auto exponent) {return pow(base, exponent); }},
-	{'$', [](auto base, auto exponent) {return pow(base, 1.0 / exponent); }}
-};
+    {'^', [](auto base, auto exponent) { return pow(base, exponent); }},
+    {'$', [](auto base, auto exponent) { return pow(base, 1.0 / exponent); }}};
 
 bool isInteger(double n);
 bool isItCorrectNumber(std::string input);
